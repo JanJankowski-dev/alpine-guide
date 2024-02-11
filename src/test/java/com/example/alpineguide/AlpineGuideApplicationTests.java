e1a417c6ba6e3c3d860d0bfa3c575605450b15e5
Jan Jankowski <j.jankowski91@gmail.com>
2024-02-11 14:05:27 +0100

    init

---
+++ src/test/java/com/example/alpineguide/AlpineGuideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.alpineguide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlpineGuideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
