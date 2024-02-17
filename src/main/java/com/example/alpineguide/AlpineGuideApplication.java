b33fef4b397c1e03e5b26e96b78a4133e6daba76
Jan Jankowski <j.jankowski91@gmail.com>
2024-02-17 18:47:21 +0100

    changes

---
+++ src/main/java/com/example/alpineguide/AlpineGuideApplication.java
@@ -0,0 +1,13 @@
+package com.example.alpineguide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlpineGuideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlpineGuideApplication.class, args);
+	}
+
+}
